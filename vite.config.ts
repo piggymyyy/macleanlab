94b5fdecd2802066b43ed09a35db60638c942abb
piggymyyy <michelleyinscience@gmail.com>
2025-04-14 16:38:04 +0100

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/macleanlab/', // 👈 This is the fix!
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
